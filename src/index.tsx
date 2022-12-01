0935abe2144c96987e75b0879f80892db4d8a205
ariovaldo <ariovaldo@ituring.com.br>
2022-12-01 20:16:03 -0300

    another try

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header from './components/Header/index';
+// import Header from './components/Header/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
+    {/* <Header /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
