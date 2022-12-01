98197e00da005a38f46fbcb3a3d007141fd32c53
ariovaldo <ariovaldo@ituring.com.br>
2022-12-01 20:12:30 -0300

    fix import file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header from './components/Header';
+import Header from './components/Header/index';
 
 ReactDOM.render(
   <React.StrictMode>
